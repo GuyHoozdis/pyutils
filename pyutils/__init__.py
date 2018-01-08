0167ae2aaf748b0f645f4233f9cd5eafa02a11d1
Scott Sullivan <scott.sullivan@tabbedout.com>
2018-01-08 11:12:23 -0600

    Expose utility functions at top-level API

--- pyutils/__init__.py
+++ pyutils/__init__.py
@@ -1,2 +1,11 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+
+__all__ = [
+    # REPL/iPDB console utilities
+    'ppd', 'ppl', 'pi',
+]
+
+
+from pyutils.terminal.format.prepr import ppd, ppl, pi
